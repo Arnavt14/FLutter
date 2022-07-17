525c7405aaea904040cde73a519d630c040f009d
Arnav Tyagi <e20cse065@bennett.edu.in>
2022-07-17 13:03:52 +0530

    final

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -15,7 +15,7 @@ void main() {
         backgroundColor: Colors.blueAccent,
         body: Center(
           child: Image(
-              image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5BHiIjuWc0uqGXPtVicK3ZsMSnX57r3xCRQ&usqp=CAU')
+              image: AssetImage('images/cat.jfif')
           ),
         ),
       ),
